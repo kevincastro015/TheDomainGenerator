13cf1c090c057b7671e9c48031e6cab9aa7f69a1
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-17 00:58:58 +0000

    DomainGenButton

--- app.js
+++ app.js
@@ -6,19 +6,14 @@ let noun = ['jogger','racoon'];
   let arr = [];
       for(let i=0; i <pronoun.length; i++) {
           for (let j=0; j < adj.length; j++) {
-              for (let n= 0; n < noun.length; n++) {
-           
+              for (let k = 0; k < noun.length; k++) {
                 let newText = document.createElement("P")
-                newText.innerText= pronoun[i]+adj[j]+noun[n]+".com"
+                newText.innerText = pronoun[i]+adj[j]+noun[k]+".com"
 //                 arr.push(pronoun[i] + adj[j] + noun[n] +".com");
-                document.queryselector(".theDomainGenerator").appendchild(newText)
+                document.getElementById("theDomainGenerator").appendchild(newText)
                   
               }
           }
       }
 //   document.querySelector(".theDomainGenerator").innerHTML = arr
-  }
-
-    
-
-
+    }
