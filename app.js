dd2d05ef170301db2fc263aa7ad39e3be2bae9dd
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-17 00:44:12 +0000

    appendchild

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
- let pronoun = ['the','our'];
-  let adj = ['great', 'big' ];
-  let noun = ['jogger','racoon'];
+function domainName() { 
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
 
-  function domainName () {
-      let arr = []
+  let arr = [];
       for(let i=0; i <pronoun.length; i++) {
           for (let j=0; j < adj.length; j++) {
               for (let n= 0; n < noun.length; n++) {
-                  arr.push(pronoun[i] + adj[j] + noun[n] +".com");
+           
+                let newText = document.createElement("P")
+                newText.innerText= pronoun[i]+adj[j]+noun[n]+".com"
+//                 arr.push(pronoun[i] + adj[j] + noun[n] +".com");
+                document.queryselector(".theDomainGenerator").appendchild(newText)
                   
               }
           }
       }
+//   document.querySelector(".theDomainGenerator").innerHTML = arr
   }
-  document.querySelector("#theDomainGenerator").innerHTML = arr
+
     
 
 
