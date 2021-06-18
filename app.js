85acd80a2d3bb7224dae297fd9dcc8440d7dd763
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-18 21:41:04 +0000

    TheDomainGenerator6.18

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function domainName() { 
+
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
@@ -7,13 +7,9 @@ let noun = ['jogger','racoon'];
       for(let i=0; i <pronoun.length; i++) {
           for (let j=0; j < adj.length; j++) {
               for (let k = 0; k < noun.length; k++) {
-                let newText = document.createElement("P")
-                newText.innerText = pronoun[i]+adj[j]+noun[k]+".com"
-//                 arr.push(pronoun[i] + adj[j] + noun[n] +".com");
-                document.getElementById("theDomainGenerator").appendchild(newText)
+                console.log(pronoun[i]+adj[i]+noun[i]+".com")
                   
               }
           }
       }
-//   document.querySelector(".theDomainGenerator").innerHTML = arr
-    }
+
